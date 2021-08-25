0e4e134260c317454ac532101f27dc9d988c8366
Hoangthong97 <Hoangthong97.th@gmail.com>
2021-08-25 23:13:08 +0700

    Add Showtag function

---
+++ Models/Tag.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAS.WebApp.Models
+{
+	public class Tag
+	{
+		public void ShowTag()
+		{
+			Console.WriteLine("Show tag function");
+		}
+	}
+}
