9b3339573a7b26a641c423c4065ee195e963faf0
Hoangthong97 <Hoangthong97.th@gmail.com>
2021-08-26 09:04:54 +0700

    add promote

---
+++ Models/Promote.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAS.WebApp.Models
+{
+	public class Promote
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
