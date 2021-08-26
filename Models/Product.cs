85b698a0088ef5143f0e0d4d95b4acdd320b7f11
Hoangthong97 <Hoangthong97.th@gmail.com>
2021-08-26 08:36:45 +0700

    add quantity property

--- Models/Product.cs
+++ Models/Product.cs
@@ -10,6 +10,7 @@ namespace SAS.WebApp.Models
 		public int Id { get; set; }
 		public string Name { get; set; }
 		public string CodeDiscount { get; set; }
+		public int Quantity { get; set; }
 		public Product()
 		{
 
